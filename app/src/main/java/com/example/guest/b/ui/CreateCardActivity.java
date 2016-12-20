e16242286c0a3f5475d63528eefc3b4deaca4264
TJ2001 <jung.m.tim@gmail.com>
2016-12-19 16:49:06 -0800

    setup base for project

---
+++ app/src/main/java/com/example/guest/b/ui/CreateCardActivity.java
@@ -0,0 +1,18 @@
+package com.example.guest.b.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.b.R;
+
+public class CreateCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_card);
+    }
+
+//    TODO take input from three text fields, front, back, deck, save object to firebase
+
+}
